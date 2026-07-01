3d261b39c0dbe800109df43b0e2d532004473045
Manikanta <manikanta.sai.37.3@mountblue.tech>
2026-07-01 11:57:02 +0530

    done change

--- questions/signals.py
+++ questions/signals.py
@@ -1,14 +1,11 @@
 from django.db.models.signals import m2m_changed, post_delete, post_save
 from django.dispatch import receiver
-
 from .models import Question
 from .search import index_question, remove_question
-
 
 @receiver(post_save, sender=Question)
 def update_question_index(sender, instance, **kwargs):
     index_question(instance)
-
 
 @receiver(m2m_changed, sender=Question.tags.through)
 def update_question_tags_in_index(
